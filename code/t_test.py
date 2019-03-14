ce59f8998bc157cbd42fe14cfbd87bc670449ad7
Eduardo Rocha <edu181294@gmail.com>
2019-03-13 19:32:09 -0500

    removed pdfs

--- code/t_test.py
+++ code/t_test.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3.6
-# chmod +x fileName.py
-
 ######################################################################################
-# CS 760, Spring 2019
-# HW 2
 # Eduardo Moura Cirilo Rocha, mouracirilor@wisc.edu
 # March of 2019
-# Problem 3: t-test
+# t-test
 ######################################################################################
 
 import json
@@ -14,24 +9,29 @@ import numpy as np
 import sys
 import math
 
+from random import shuffle
 from bayes_function import bayes
-
 
 
 trainingSetPath = "../datasets/tic-tac-toe.json"
 testSetPath = "../datasets/tic-tac-toe.json"
 
+# Load data
 with open(trainingSetPath) as f:
     trainSet = json.load(f)
-
 # Load test set
 with open(testSetPath) as f:
     testSet = json.load(f)
-
 Data = trainSet["data"]
 foldSize = round((len(Data))/10)
 
+# randomly shuffle data before separating folds
+shuffle(Data)
 
+# Print file header
+print("TAN NaiveBayes")
+
+# create fold and apply classifier
 for i in range(10):
 	testData = []
 	trainingData = []
@@ -43,22 +43,24 @@ for i in range(10):
 		else:
 			trainingData.append(Data[j])
 
+	# TAN
 	results_t = bayes(trainingSetPath, testSetPath, "t", trainingData, testData)
-	print(results_t[0])
 	predictions = [probability >= 0.5 for probability in results_t[1]]
-
 	accuracy_t = sum([predictions[i] == results_t[0][i] \
 		for i in range(len(predictions))])/len(predictions)
 
-
-
-	print(accuracy_t)
-
-
-	#accuracy_t.append([])
-
+	# Naive Bayes
 	results_n = bayes(trainingSetPath, testSetPath, "n", trainingData, testData)
+	predictions = [probability >= 0.5 for probability in results_n[1]]
+	accuracy_n = sum([predictions[i] == results_n[0][i] \
+		for i in range(len(predictions))])/len(predictions)
 
+	# print accuracy results
+	print("%.4f"%accuracy_t, end = " ")
+	print("&", end = " ")
+	print("%.4f"%accuracy_n, end = " ")
+	print("&", end = " ")
+	print("%.4f"%(accuracy_t - accuracy_n), end = "\\\\ \n")
 
 
 
